1c5d5508ba89918eb4d83971851912206516c7dc
gechderib <gechderib@gmail.com>
2023-05-11 09:01:13 +0300

    adding agent and user

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,4 @@
-const ROLES = ["sse","farmer","customer"]
+const ROLES = ["sse","farmer","customer","admin","agent"]
 const ROLE2 = ["agent", "admin","sse","farmer","customer"]
 
 module.exports = {ROLES, ROLE2}
