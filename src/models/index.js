801f266ca2f43846283bddef8b7c0a3c9477f75e
gechderib <gechderib@gmail.com>
2023-06-01 12:44:55 +0300

    payment status

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,5 @@
 const ROLES = ["sse","farmer","customer","admin","agent"]
 const ROLE2 = ["agent", "admin","sse","farmer","customer"]
-const Status = ["pending", "accepted", "rejected"]
+const Status = ["pending", "accepted", "rejected","completed"]
+
 module.exports = {ROLES, ROLE2, Status}
